282330ec2bfd064c7fbabe91d7516460cd1e64e1
Xstudios2019 <xstudios383@gmail.com>
2023-02-14 22:13:26 -0600

    Trying to fix /spawn :(

--- src/main/java/net/xst/RawGensPlugin/commands/Spawn/spawn_command.kt
+++ src/main/java/net/xst/RawGensPlugin/commands/Spawn/spawn_command.kt
@@ -11,19 +11,24 @@ import org.bukkit.entity.Player
 class spawn_command @Inject constructor(private val plugin: RawGensCorePlugin) {
 	@CommandMethod("spawn")
 	@CommandDescription("Takes you to spawn")
-	suspend fun onspawn(player: Player){
+	suspend fun onspawn(player: Player) {
 		val xCord = plugin.config.getDouble("rawgens.spawn.x")
 		val yCord = plugin.config.getDouble("rawgens.spawn.y")
 		val zCord = plugin.config.getDouble("rawgens.spawn.z")
 		val pitch = plugin.config.getDouble("rawgens.spawn.pitch")
 		val yaw = plugin.config.getDouble("rawgens.spawn.yaw")
-		val world_name = plugin.config.getString("rawgens.spawn.world")!!
-		val location = Location(Bukkit.getWorld(world_name), xCord, yCord, zCord, yaw.toFloat(), pitch.toFloat())
-		if(!player.teleport(location)){
-			plugin.logger.info("Error teleporting")
+		val worldName = plugin.config.getString("rawgens.spawn.world")
+
+		val world = Bukkit.getWorld(worldName!!)
+		if (world == null) {
+			plugin.logger.warning("Invalid world name specified in config: $worldName")
+			return
 		}
 
-
+		val location = Location(world, xCord, yCord, zCord, yaw.toFloat(), pitch.toFloat())
+		if (!player.teleport(location)) {
+			plugin.logger.warning("Error teleporting player to spawn")
+		}
 	}
 
 }
